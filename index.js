648767f500183dd7e06e5e4bc0fd8a8105c247c6
Robyrogers <rb147258@gmail.com>
2019-03-10 10:50:51 +0600

    Added redux package & store and testing

---
+++ index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import {
+    addTodo,
+    toggleTodo,
+    setVisibilityFilter,
+    VisibilityFilters
+} from './actions'
+
+const store = createStore(todoApp)
+
+const unsubsscribe = store.subscribe(()=>console.log(store.getState()))
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+unsubscribe()
