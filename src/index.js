dd6a7fb37c2cb9fe13422b9b54a0c8e74f7d7e60
Robyrogers <rb147258@gmail.com>
2019-03-11 01:55:00 +0600

    final fix for sure

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todoApp from './reducers'
-import {
-    addTodo,
-    toggleTodo,
-    setVisibilityFilter,
-    VisibilityFilters
-} from './actions'
+import rootReducer from './reducers'
+import App from './components/App'
 
-const store = createStore(todoApp)
+const store = createStore(rootReducer)
 
-const unsubscribe = store.subscribe(()=>console.log(store.getState()))
-
-// Dispatch some actions
-store.dispatch(addTodo('Learn about actions'))
-store.dispatch(addTodo('Learn about reducers'))
-store.dispatch(addTodo('Learn about store'))
-store.dispatch(toggleTodo(0))
-store.dispatch(toggleTodo(1))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-// Stop listening to state updates
-unsubscribe()
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
